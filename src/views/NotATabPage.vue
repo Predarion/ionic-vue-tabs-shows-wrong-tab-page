5ee8d3e5ca09bbcb16f6036a588e94498c751690
Manuel Göster <manuel.goester@ambalytics.com>
2021-12-10 08:37:15 +0100

    adds reproduction

---
+++ src/views/NotATabPage.vue
@@ -0,0 +1,52 @@
+<template>
+  <ion-page>
+    <ion-header>
+      <ion-toolbar>
+        <ion-title>Not a tab page</ion-title>
+      </ion-toolbar>
+    </ion-header>
+    <ion-content :fullscreen="true">
+      <ion-header collapse="condense">
+        <ion-toolbar>
+          <ion-title size="large">Not a tab page</ion-title>
+        </ion-toolbar>
+      </ion-header>
+      <div>
+        <RouterLink to="/not-a-tab-page/subpage">
+          Route to /not-a-tab-page/subpage
+        </RouterLink>
+      </div>
+      <div>
+        <RouterLink to="/tabs/tab2"> Route to /tabs/tab2 </RouterLink>
+      </div>
+      <div>
+        <RouterLink to="/tabs/tab2/subpage">
+          Route to /tabs/tab2/subpage
+        </RouterLink>
+      </div>
+    </ion-content>
+  </ion-page>
+</template>
+
+<script lang="ts">
+import {
+  IonPage,
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonContent,
+} from "@ionic/vue"
+import { RouterLink } from "vue-router"
+
+export default {
+  name: "Tab1",
+  components: {
+    IonHeader,
+    IonToolbar,
+    IonTitle,
+    IonContent,
+    IonPage,
+    RouterLink,
+  },
+}
+</script>
